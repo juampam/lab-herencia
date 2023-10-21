ac492a2ab70733c7a64c04d8d3a8af60bfdce37e
juampam <jpablomuralles@gmail.com>
2023-10-21 00:46:26 -0600

    fix code

---
+++ Inventario.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class Inventario {
+    private ArrayList<Categoria> categorias;
+
+    public Inventario() {
+        this.categorias = new ArrayList<>();
+    }
+
+    public void agregarCategoria(Categoria categoria) {
+        categorias.add(categoria);
+    }
+
+    public void listarCategorias() {
+        for (Categoria categoria : categorias) {
+            System.out.println("Categoría: " + categoria.getNombre());
+            categoria.listarProductos();
+        }
+    }
+
+    public Categoria encontrarCategoriaPorNombre(String nombre) {
+        for (Categoria categoria : categorias) {
+            if (categoria.getNombre().equals(nombre)) {
+                return categoria;
+            }
+        }
+        return null; 
+
+}
