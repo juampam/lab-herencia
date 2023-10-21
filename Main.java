ff02b62eef83da8cb012a7f3314b2c01a60fc21d
juampam <jpablomuralles@gmail.com>
2023-10-21 01:06:31 -0600

    add csv example

--- Main.java
+++ Main.java
@@ -1,35 +1,54 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        // Crear un inventario
-        Inventario inventario = new Inventario();
+        Inventario inventario = new Inventario(); 
+        cargarDatosDesdeCSV(inventario, "datos.csv");
 
-        // Crear categorías
-        Categoria pantalon = new Categoria("Ropa");
-        Categoria blusa = new Categoria("Joyas");
-        Categoria camisa = new Categoria("Joyas");
-        Categoria collar = new Categoria("Joyas");
-
-        // Crear productos
-        Producto pantalon1 = new Pantalon(1, "Pantalón Casual", 10, 5, "disponible", 39.99, "M", "lona");
-        Producto pantalon2 = new Pantalon(2, "Pantalón Deportivo", 15, 8, "disponible", 29.99, "L", "jogger");
-        Producto blusa1 = new BlusaCamisa(3, "Blusa Elegante", 20, 10, "disponible", 49.99, "M", "rojo");
-        Producto camisa1 = new BlusaCamisa(3, "Blusa Elegante", 20, 10, "disponible", 49.99, "M", "rojo");
-        Producto collar1 = new Collares(4, "Collar de Plata", 5, 3, "disponible", 79.99, "única", "plata");
-
-        // Agregar productos a las categorías
-        pantalon.agregarProducto(pantalon1);
-        pantalon.agregarProducto(pantalon2);
-        blusa.agregarProducto(blusa1);
-        camisa.agregarProducto(camisa1);
-        collar.agregarProducto(collar1);
-
-        // Agregar categorías al inventario
-        inventario.agregarCategoria(pantalon);
-        inventario.agregarCategoria(blusa);
-        inventario.agregarCategoria(camisa);
-
-        // Listar categorías y productos
         System.out.println("Inventario:");
         inventario.listarCategorias();
+    }
+
+    public static void cargarDatosDesdeCSV(Inventario inventario, String archivoCSV) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(archivoCSV))) {
+            String linea;
+            boolean primeraLinea = true; 
+
+            while ((linea = reader.readLine()) != null) {
+                if (primeraLinea) {
+                    primeraLinea = false;
+                    continue;
+                }
+
+                String[] campos = linea.split(",");
+                if (campos.length == 11) { 
+                    int idProducto = Integer.parseInt(campos[0]);
+                    String nombre = campos[1];
+                    int cantidadDisponible = Integer.parseInt(campos[2]);
+                    int cantidadVendidos = Integer.parseInt(campos[3]);
+                    String estado = campos[4];
+                    double precio = Double.parseDouble(campos[5]);
+                    String categoria = campos[6];
+                    String talla = campos[7];
+                    String tipo = campos[8];
+                    String color = campos[9];
+                    String material = campos[10];
+
+                    Producto producto = new Producto(idProducto, nombre, cantidadDisponible, cantidadVendidos, estado, precio);
+
+                    Categoria categoriaExistente = inventario.encontrarCategoriaPorNombre(categoria);
+                    if (categoriaExistente == null) {
+                        categoriaExistente = new Categoria(categoria);
+                        inventario.agregarCategoria(categoriaExistente);
+                    }
+                    categoriaExistente.agregarProducto(producto);
+
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
